0c5e08505cc110084ba43558ea827593195d5a23
thembones79 <michal.szumnarski@gmail.com>
2025-01-19 16:37:31 +0100

    rust traits

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,21 @@ mod container;
 mod stack;
 
 use basket::Basket;
-use stack::Stack;
 use container::Container;
+use stack::Stack;
 
-fn add_string<T: Container<String>>(c: &mut T, s:String){
-
+fn add_string<T: Container<String>>(c: &mut T, s: String) {
+    c.put(s);
 }
 
 fn main() {
-    let b1 = Basket::new(String::from("hi there"));
+    let mut b1 = Basket::new(String::from("hi there"));
     let b2 = Basket::new(10);
     let b3 = Basket::new(true);
 
-    let s1 = Stack::new(vec![String::from("hi")]);
+    let mut s1 = Stack::new(vec![String::from("hi")]);
     let s2 = Stack::new(vec![1, 2, 3]);
+
+    add_string(&mut b1, String::from("hi"));
+    add_string(&mut s1, String::from("hi there"));
 }
